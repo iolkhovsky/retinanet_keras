69fae8d23633ce886a684ba20fc1e5fbc8164fa1
igor <i.v.olkhovsky@gmail.com>
2020-11-06 09:52:42 +0300

    webcam deploy added

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,3 +1,4 @@
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -29,3 +30,19 @@ def visualize_detections(
         )
     plt.show()
     return ax
+
+
+def visualize_detections_img(
+    image, boxes, classes, scores, figsize=(7, 7), linewidth=1, color=[0, 0, 1]
+):
+    """Visualize Detections"""
+    image = np.array(image, dtype=np.uint8)
+    for box, _cls, score in zip(boxes, classes, scores):
+        text = "{}: {:.2f}".format(_cls, score)
+        x1, y1, x2, y2 = box
+        w, h = x2 - x1, y2 - y1
+        image = cv2.rectangle(img=image, pt1=(x1, y1), pt2=(x2, y2), color=(0, 0, 255),
+                          thickness=2)
+        image = cv2.putText(image, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255),
+                        1, cv2.LINE_AA)
+    return image
